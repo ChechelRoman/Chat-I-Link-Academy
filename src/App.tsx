6319d0dbc288b0ab9f942be616dd0a307a25a745
Chechel Roman <chechel-roman@lenta.ru>
2021-10-19 17:00:42 +0700

    feat: add input component with types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import Input from './components/atoms/input';
+import { useState } from 'react';
 
 function App() {
-  return <div>Hello World</div>;
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  };
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <React.Fragment>
+      <Input
+        id="name"
+        name="name"
+        placeholder="Input user name"
+        value={userName}
+        handler={handleName}
+      />
+      <Input
+        id="password"
+        name="password"
+        placeholder="Input password"
+        value={password}
+        handler={handlePassword}
+      />
+    </React.Fragment>
+  );
 }
 
 export default App;
