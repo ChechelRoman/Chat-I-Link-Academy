76f529ae12b7beddafb3b0d22e821bfe3afc0f2b
Chechel Roman <chechel-roman@lenta.ru>
2021-10-21 22:37:22 +0700

    fix: refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,9 @@
 import React from 'react';
-import Input from './components/atoms/Input';
-import Button from './components/atoms/Button';
-import { useState } from 'react';
+import LoginPage from './components/pages/LoginPage';
+import './style.scss';
 
 function App() {
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [isValid, setValid] = useState(false);
-
-  const handleName: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setUserName(event.target.value);
-  };
-
-  const handlePassword: React.ChangeEventHandler<HTMLInputElement> = (
-    event
-  ) => {
-    setPassword(event.target.value);
-  };
-
-  return (
-    <React.Fragment>
-      <Input
-        id="name"
-        name="name"
-        placeholder="Input user name"
-        value={userName}
-        onChange={handleName}
-      />
-      <Input
-        id="password"
-        name="password"
-        placeholder="Input password"
-        value={password}
-        onChange={handlePassword}
-      />
-      <Button type="submit" disabled={!isValid}>
-        Log in
-      </Button>
-    </React.Fragment>
-  );
+  return <LoginPage />;
 }
 
 export default App;
-
-// const formField = (props) => {
-//   return (
-//     <label>
-//       <span>{label}</span>
-//       <Input />
-//       {error ? <span>{error}</span> : null}
-//     </label>
-//   )
-// }
