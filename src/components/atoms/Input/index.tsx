cd6be7109efceeb6e61caf2be07090ee81b28cca
Chechel Roman <chechel-roman@lenta.ru>
2021-10-21 19:47:40 +0700

    fix: refactor Input component

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
+
 import './style.scss';
+
 import cn from 'classnames';
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = (props: InputProps) => {
   const { className, ...otherProps } = props;
 
   const classes = cn(className, 'input');
+
   return <input className={classes} {...otherProps} />;
 };
 
