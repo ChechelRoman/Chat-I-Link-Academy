10cd0a461e9ca7dcbff4e1470e39f38f87fca24a
Chechel Roman <chechel-roman@lenta.ru>
2021-10-28 23:28:24 +0700

    feat: add adaptive for 375px width screen

--- src/components/pages/LoginPage/index.tsx
+++ src/components/pages/LoginPage/index.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 import './style.scss';
 import { LogInForm } from '../../organisms/LogInForm';
 import { LoginTemplate } from '../../templates/LoginTemplate';
-import loginDecor from '../../../images/login-decor.png';
 
 export const LoginPage: React.FC = () => {
-  return (
-    <LoginTemplate
-      left={<LogInForm />}
-      right={<img src={loginDecor} alt="login page decor"></img>}
-    />
-  );
+  return <LoginTemplate left={<LogInForm />} />;
 };
