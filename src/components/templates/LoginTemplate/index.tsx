3e91c037601c06a79208f34a95cd8328bfd2b235
Chechel Roman <chechel-roman@lenta.ru>
2021-10-28 23:28:24 +0700

    feat: add adaptive for 375px width screen

--- src/components/templates/LoginTemplate/index.tsx
+++ src/components/templates/LoginTemplate/index.tsx
@@ -3,17 +3,13 @@ import './style.scss';
 
 type LoginPageTempalateProps = {
   left: React.ReactNode | React.ReactNode[];
-  right: React.ReactNode | React.ReactNode[];
 };
 
-export const LoginTemplate: React.FC<LoginPageTempalateProps> = ({
-  left,
-  right,
-}) => {
+export const LoginTemplate: React.FC<LoginPageTempalateProps> = ({ left }) => {
   return (
     <div className="login-page-template">
       <div className="login-page-template__left">{left}</div>
-      <div className="login-page-template__right">{right}</div>
+      <div className="login-page-template__right"></div>
     </div>
   );
 };
