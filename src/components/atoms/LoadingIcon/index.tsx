5897cc82dce21ffe38642f8495e06b74e23eeabb
Chechel Roman <chechel-roman@lenta.ru>
2021-10-28 21:39:53 +0700

    feat: add LoadingIcon component

---
+++ src/components/atoms/LoadingIcon/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.scss';
+import loadingIcon from '../../../images/loading-icon.png';
+
+export const LoadingIcon: React.FC = () => {
+  return (
+    <div className="loading-icon-container">
+      <img className="loading-icon" src={loadingIcon} alt="loading icong" />
+    </div>
+  );
+};
