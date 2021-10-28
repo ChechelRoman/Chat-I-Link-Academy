abca4b167d6a8decee97b7d404949dafc7b432c6
Chechel Roman <chechel-roman@lenta.ru>
2021-10-28 21:50:02 +0700

    refactor: refactor LogInForm component

--- src/components/organisms/LogInForm/index.tsx
+++ src/components/organisms/LogInForm/index.tsx
@@ -1,42 +1,83 @@
 import React from 'react';
-import { FormField } from '../../molecules/FormField';
+import { ErrorIcon } from '../../atoms/ErrorIcon';
 import { Logo } from '../../atoms/Logo';
 import { Button } from '../../atoms/Button';
 import { Header1, Header2 } from '../../atoms/Typography';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import './style.scss';
+import cn from 'classnames';
 
-type LogInFormProps = {
-  type: 'login-logo' | 'chat-logo';
-};
+interface IFormInputs {
+  userName: string;
+  password: string;
+}
 
-export const LogInForm: React.FC<LogInFormProps> = ({ type }) => {
-  const isValid = true;
+const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data);
+
+export const LogInForm: React.FC = () => {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = useForm<IFormInputs>();
+
+  const userNameInputclasses = cn('input', {
+    invalid: errors.userName,
+  });
+
+  const passwordInputclasses = cn('input', {
+    invalid: errors.password,
+  });
 
   return (
-    <>
-      <Logo type={type} />
+    <div className="login-form-container">
+      <Logo type="login-logo" />
       <Header1>
         Welcome to <span className="header-blue">Chatty</span>
         <span className="header-grey">!</span>
       </Header1>
       <Header2>Please, authorize yourself</Header2>
-      <form action="post">
-        <FormField
-          isValid={isValid}
-          label="User name"
-          name="user"
-          placeholder="Input user name"
-          error=""
-        />
-        <FormField
-          isValid={isValid}
-          label="Password"
-          name="password"
-          placeholder="Input password"
-          error=""
-        />
-        <Button type="submit">Log in</Button>
+      <form action="post" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-field">
+          <label>
+            <span className="form-field__label">User name</span>
+            <div className="form-field__input">
+              <input
+                {...register('userName', { required: true, minLength: 2 })}
+                className={userNameInputclasses}
+                placeholder="Input user name"
+              />
+              {errors.userName ? <ErrorIcon /> : null}
+            </div>
+          </label>
+          <span className="form-field__error">
+            {errors.userName &&
+              'This field should be minimum 2 characters long'}
+          </span>
+        </div>
+
+        <div className="form-field">
+          <label>
+            <span className="form-field__label">Password</span>
+            <div className="form-field__input">
+              <input
+                {...register('password', { required: true, minLength: 2 })}
+                className={passwordInputclasses}
+                placeholder="Input password"
+              />
+              {errors.password ? <ErrorIcon /> : null}
+            </div>
+          </label>
+          <span className="form-field__error">
+            {errors.password &&
+              'This field should be minimum 2 characters long'}
+          </span>
+        </div>
+
+        <Button type="submit" isFormValid={isValid}>
+          Log in
+        </Button>
       </form>
-    </>
+    </div>
   );
 };
