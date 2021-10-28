7da691941f1fc8b0e580dcb5111e048d3483be29
Chechel Roman <chechel-roman@lenta.ru>
2021-10-28 21:40:39 +0700

    feat: add ChatBodyHeader component

---
+++ src/components/molecules/ChatBodyHeader/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.scss';
+import { Header3, Text1 } from '../../atoms/Typography';
+
+type ContactHeaderProps = {
+  lastSeen: string;
+  contactName: string;
+};
+
+export const ChatBodyHeader: React.FC<ContactHeaderProps> = ({
+  lastSeen,
+  contactName,
+}) => {
+  return (
+    <div className="contact-header">
+      <Header3>{contactName}</Header3>
+      <Text1>Last seen {lastSeen} minutes ago</Text1>
+    </div>
+  );
+};
