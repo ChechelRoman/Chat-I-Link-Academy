067f40ce187b72774f87b7842397753d390f51dd
Chechel Roman <chechel-roman@lenta.ru>
2021-11-09 23:39:29 +0700

    feat: add sign_in route to Router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,16 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { SCREENS } from './endpoints';
 import { LoginPage } from '../components/pages/LoginPage';
 import { ChatPage } from '../components/pages/ChatPage';
+import { SignInPage } from '../components/pages/SignInPage';
 
 export const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path={SCREENS.SCREEN_LOGIN} exact>
         <LoginPage />
+      </Route>
+      <Route path={SCREENS.SCREEN_SIGN} exact>
+        <SignInPage />
       </Route>
       <Route path={SCREENS.SCREEN_CHAT} exact>
         <ChatPage />
