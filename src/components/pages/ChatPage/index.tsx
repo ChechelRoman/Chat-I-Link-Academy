5db6c46f64ddcd6089e0515f32a3ed4fb9a955ef
Chechel Roman <chechel-roman@lenta.ru>
2021-11-09 23:34:57 +0700

    feat: add websocket connectio to ChatPage component

--- src/components/pages/ChatPage/index.tsx
+++ src/components/pages/ChatPage/index.tsx
@@ -4,42 +4,98 @@ import { ChatTemplate } from '../../templates/ChatTemplate';
 import { ChatBody } from '../../organisms/ChatBody';
 import { ChatHeader } from '../../molecules/ChatHeader';
 import { Contacts } from '../../organisms/Contacts';
-import { emptyResponse, filledResponse } from './mocks';
 import { NoUsersMessage } from '../../molecules/NoUsersMessage';
 import { LoadingIcon } from '../../atoms/LoadingIcon';
+import { useEffect } from 'react';
+// import { emptyResponse } from './mocks';
+
+const websocket = new WebSocket(
+  `ws://109.194.37.212:2346/?type=test&ws_id=${localStorage.getItem(
+    'connect_key'
+  )}`
+);
+
+export interface UserList {
+  name: string;
+  gender: string;
+}
 
 export const ChatPage: React.FC = () => {
-  // const response = emptyResponse;
-  const response = filledResponse;
+  const [list, setList] = useState<UserList[]>([]);
+  const [isOpened, setIsOpened] = useState<boolean>(false);
   const [currentChatId, setcurrentChatId] = useState<string>('empty');
 
-  if (response.length === 0) {
+  useEffect(() => {
+    websocket.onopen = () => setIsOpened(true);
+    websocket.onclose = () => setIsOpened(false);
+    websocket.onerror = (error) => console.log(error);
+  }, []);
+
+  useEffect(() => {
+    if (websocket.readyState === 1) {
+      setIsOpened(true);
+      websocket.send(JSON.stringify({ type: 'users_list' }));
+      websocket.onmessage = (msg) => {
+        const list = JSON.parse(msg.data);
+        setList(list.data);
+      };
+    }
+  }, [isOpened]);
+
+  // const response = emptyResponse;
+
+  if (!isOpened) {
+    return (
+      <ChatTemplate
+        header={<ChatHeader />}
+        chatBody={<LoadingIcon />}
+        isActive={currentChatId}
+      />
+    );
+  }
+
+  if (isOpened && list.length === 0) {
     return (
       <ChatTemplate
         header={<ChatHeader />}
         contactsBar={
           <NoUsersMessage description="There is no other users yet" />
         }
-        /*
-          Не придумал пока как вставить эту анимацию при получении response,
-          оставлю пока здесь :)
-        */
-        chatBody={<LoadingIcon />}
+        isActive={currentChatId}
       />
     );
   }
+
+  // if (response.length === 0) {
+  //   return (
+  //     <ChatTemplate
+  //       header={<ChatHeader />}
+  //       contactsBar={
+  //         <NoUsersMessage description="There is no other users yet" />
+  //       }
+  //       isActive={currentChatId}
+  //     />
+  //   );
+  // }
 
   return (
     <ChatTemplate
       contactsBar={
         <Contacts
-          chats={response}
+          chats={list}
           onClick={setcurrentChatId}
           currentChatId={currentChatId}
         />
       }
       header={<ChatHeader />}
-      chatBody={<ChatBody chats={response} currentChatId={currentChatId} />}
+      chatBody={
+        <ChatBody
+          chats={list}
+          currentChatId={currentChatId}
+          onClick={setcurrentChatId}
+        />
+      }
+      isActive={currentChatId}
     />
   );
 };
