345757b185f67562ea64e87c00408c4b3335517d
Chechel Roman <chechel-roman@lenta.ru>
2021-11-09 23:37:10 +0700

    feat: add adaptive for width 375px for ChatPage

--- src/components/templates/ChatTemplate/index.tsx
+++ src/components/templates/ChatTemplate/index.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
+import cn from 'classnames';
 import './style.scss';
 
 type ChatTemplateProps = {
   header?: React.ReactNode | React.ReactNode[];
   contactsBar?: React.ReactNode | React.ReactNode[];
   chatBody?: React.ReactNode | React.ReactNode[];
+  isActive: string;
 };
 
 export const ChatTemplate: React.FC<ChatTemplateProps> = ({
   header,
   contactsBar,
   chatBody,
+  isActive,
 }) => {
+  const headerClassnames = cn('header-wrapper', {
+    chosen: isActive !== 'empty',
+  });
+
+  const contactsClassnames = cn('contacts-wrapper', {
+    chosen: isActive !== 'empty',
+  });
+
+  const mainClassnames = cn('main-wrapper', {
+    chosen: isActive !== 'empty',
+  });
+
   return (
     <div className="chat-template">
-      <div className="header">{header}</div>
+      <div className={headerClassnames}>
+        <div className="header">{header}</div>
+      </div>
       <div className="content-wrapper">
-        <div className="contacts-bar">{contactsBar}</div>
-        <div className="main">{chatBody}</div>
+        <div className={contactsClassnames}>
+          <div className="contacts-bar">{contactsBar}</div>
+        </div>
+        <div className={mainClassnames}>
+          <div className="main">{chatBody}</div>
+        </div>
       </div>
     </div>
   );
