c15d863318cf614cab36193f24a838c0842917ea
Chechel Roman <chechel-roman@lenta.ru>
2021-11-09 23:29:38 +0700

    refactor: refactor ChatBodyHeader component

--- src/components/molecules/ChatBodyHeader/index.tsx
+++ src/components/molecules/ChatBodyHeader/index.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import './style.scss';
 import { Header3, Text1 } from '../../atoms/Typography';
-
+import { UserAvatar } from '../../atoms/UserAvatar';
+import backArrow from '../../../images/back-arrow-icon.png';
 interface ContactHeaderProps {
-  lastSeen: string;
+  // lastSeen: string;
   contactName: string;
+  onClick: (currentChatId: string) => void;
+  gender: string;
 }
 
 export const ChatBodyHeader: React.FC<ContactHeaderProps> = ({
-  lastSeen,
+  // lastSeen,
   contactName,
+  onClick,
+  gender,
 }) => {
+  const handleClick = () => {
+    onClick('empty');
+  };
+
   return (
     <div className="contact-header">
+      <img
+        className="back-arrow"
+        src={backArrow}
+        alt="back arrow icon"
+        onClick={handleClick}
+      />
+      <UserAvatar gender={gender} />
       <Header3>{contactName}</Header3>
-      <Text1>Last seen {lastSeen} minutes ago</Text1>
+      {/* <Text1>Last seen {lastSeen} minutes ago</Text1> */}
     </div>
   );
 };
