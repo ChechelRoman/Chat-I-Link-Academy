7da42268d3b8a54f89d59b4586f9030445ce7e9d
Chechel Roman <chechel-roman@lenta.ru>
2021-11-20 16:47:51 +0700

    feat: add mobx store

---
+++ src/store/chats.ts
@@ -0,0 +1,50 @@
+import { makeAutoObservable } from 'mobx';
+import { uniqueId, sample } from 'lodash';
+
+export interface UserInfo {
+  id?: string;
+  name: string;
+  gender: string;
+  lastSeen?: string;
+  messages: Messages[];
+}
+
+export interface Messages {
+  id: string;
+  attached: boolean;
+  source: string;
+  text?: string;
+  title?: string;
+  size?: number;
+  link?: string;
+}
+
+const timeArray = ['10', '9', '8', '7', '6', '5', '4', '3', '2'];
+
+class Chats {
+  chats: UserInfo[] = [];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  addChats(userList: UserInfo[]) {
+    const response: UserInfo[] = [...userList];
+    response.forEach((item) => {
+      item.id = uniqueId();
+      item.lastSeen = sample(timeArray);
+      item.messages = [];
+    });
+    this.chats = [...response];
+  }
+
+  addMessage(id: string | undefined, message: Messages) {
+    this.chats.forEach((chat) => {
+      if (chat.id === id) {
+        chat.messages?.push(message);
+      }
+    });
+  }
+}
+
+export default new Chats();
