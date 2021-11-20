439e562779a94f70134236c828fd5c93a17cbc5d
Chechel Roman <chechel-roman@lenta.ru>
2021-11-20 21:09:16 +0700

    refactor: refactor ChatBody component

--- src/components/organisms/ChatBody/index.tsx
+++ src/components/organisms/ChatBody/index.tsx
@@ -1,104 +1,68 @@
 import React from 'react';
 import './style.scss';
-// import { useState } from 'react';
 import {
   ChatBodyHeader,
   ContactHeaderProps,
 } from '../../molecules/ChatBodyHeader';
-// import { Message } from '../../molecules/Message';
+import { Message } from '../../molecules/Message';
 import { ChatBodySendMenu } from '../../molecules/ChatBodySendMenu';
 import { Header4 } from '../../atoms/Typography';
-// import { Response } from '../../pages/ChatPage/mocks';
-import { UserList } from '../../pages/ChatPage';
+import chats from '../../../store/chats';
+import { observer } from 'mobx-react-lite';
+
 interface ChatBodyProps {
-  chats: UserList[];
+  socket: WebSocket;
   currentChatId: string;
   onClick: ContactHeaderProps['onClick'];
 }
 
-// export interface SentMessages {
-//   text?: string;
-//   type: string;
-//   source?: string;
-//   title?: string;
-//   size?: number;
-// }
+export const ChatBody: React.FC<ChatBodyProps> = observer(
+  ({ socket, currentChatId, onClick }) => {
+    if (currentChatId === undefined) {
+      return (
+        <div className="user-tip">
+          <Header4>Select a chat to start messaging</Header4>
+        </div>
+      );
+    }
 
-export const ChatBody: React.FC<ChatBodyProps> = ({
-  chats,
-  currentChatId,
-  onClick,
-}) => {
-  /*
-    В данном компоненте я использую этот хук для проверки верстки отправленных мною сообщений.
-  */
-  // const [sentMessages, setSentMessages] = useState<SentMessages[]>([]);
+    const activeChat = chats.chats.filter(
+      (chat) => chat.id === currentChatId
+    )[0];
 
-  if (currentChatId === undefined) {
+    const messageList = activeChat.messages.map((message) => {
+      return (
+        <li key={message.id}>
+          <Message
+            link={message.link}
+            attached={message.attached}
+            id={message.id}
+            source={message.source}
+            text={message.text}
+            size={message.size}
+            title={message.title}
+          />
+        </li>
+      );
+    });
+
     return (
-      <div className="user-tip">
-        <Header4>Select a chat to start messaging</Header4>
+      <div className="chat-body">
+        <div className="chat-body__header">
+          <ChatBodyHeader
+            contactName={activeChat.name}
+            onClick={onClick}
+            gender={activeChat.gender}
+            lastSeen={activeChat.lastSeen}
+          />
+        </div>
+        <div className="chat-body__messages">
+          <ul>{messageList}</ul>
+        </div>
+        <div className="chat-body__send-menu">
+          <ChatBodySendMenu socket={socket} chatId={activeChat.id} />
+        </div>
       </div>
     );
   }
-
-  const activeChat = chats.filter(
-    (chat, index) => String(index) === currentChatId
-  )[0];
-  // const messageList = activeChat.messages.map((message, index) => {
-  //   return (
-  //     <li key={index}>
-  //       <Message
-  //         type={message.type}
-  //         text={message.text}
-  //         source={message.source}
-  //         size={message.size}
-  //         title={message.title}
-  //       />
-  //     </li>
-  //   );
-  // });
-
-  /*
-    Эту константу я также сделал для проверки отправленных мной сообщений и вставил ее в рендерящийся список.
-    Я понимаю, что так делать нехорошо, тут лучше уже использовать стейт менеджер, но еще руки не дошли.
-  */
-  // const sentMessagesList = sentMessages.map((message, index) => {
-  //   return (
-  //     <li key={index}>
-  //       <Message
-  //         type={message.type}
-  //         text={message.text}
-  //         source={message.source}
-  //         title={message.title}
-  //         size={message.size}
-  //       />
-  //     </li>
-  //   );
-  // });
-
-  return (
-    <div className="chat-body">
-      <div className="chat-body__header">
-        <ChatBodyHeader
-          contactName={activeChat.name}
-          onClick={onClick}
-          gender={activeChat.gender}
-          // lastSeen={activeChat.lastSeen}
-        />
-      </div>
-      <div className="chat-body__messages">
-        {/* <ul>
-          {messageList}
-          {sentMessagesList}
-        </ul> */}
-      </div>
-      <div className="chat-body__send-menu">
-        <ChatBodySendMenu
-        // onClick={setSentMessages}
-        // sentMessages={sentMessages}
-        />
-      </div>
-    </div>
-  );
-};
+);
