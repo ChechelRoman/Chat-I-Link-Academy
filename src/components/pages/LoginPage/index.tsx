d2231c11c61a8e22c50855edf29ffa5da1c99252
Chechel Roman <chechel-roman@lenta.ru>
2021-12-02 20:10:10 +0700

    feat: add document title changing

--- src/components/pages/LoginPage/index.tsx
+++ src/components/pages/LoginPage/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './style.scss';
+import { useEffect } from 'react';
 import { LogInForm } from '../../organisms/LogInForm';
 import { AuthorizationTemplate } from '../../templates/AuthorizationTemplate';
 
 export const LoginPage: React.FC = () => {
+  useEffect(() => {
+    document.title = 'Chat Login';
+  });
   return <AuthorizationTemplate left={<LogInForm />} />;
 };
--- src/components/pages/SignInPage/index.tsx
+++ src/components/pages/SignInPage/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './style.scss';
+import { useEffect } from 'react';
 import { AuthorizationTemplate } from '../../templates/AuthorizationTemplate';
 import { SignInForm } from '../../organisms/SignInForm';
 
 export const SignInPage: React.FC = () => {
+  useEffect(() => {
+    document.title = 'Chat Sign In';
+  });
   return <AuthorizationTemplate left={<SignInForm />} />;
 };
