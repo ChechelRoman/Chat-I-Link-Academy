25b8891c11b94dd1ba6a72a6299ab2393c25df22
Chechel Roman <chechel-roman@lenta.ru>
2021-12-02 20:09:33 +0700

    fix: change shortenMessage function

--- src/utils/shortenMessage.ts
+++ src/utils/shortenMessage.ts
@@ -3,8 +3,8 @@ import { ContactInfoProps } from '../components/molecules/ContactInfo';
 export const shortenMessage = (message: ContactInfoProps['lastMessage']) => {
   let result = '';
   if (message !== undefined) {
-    if (message.length > 25) {
-      result += `${message.slice(0, 23)}...`;
+    if (message.length > 21) {
+      result += `${message.slice(0, 19)}...`;
     } else {
       return message;
     }
