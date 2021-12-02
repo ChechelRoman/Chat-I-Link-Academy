4da07df15911f1c4d013017fe7934f034b2d1bb0
Chechel Roman <chechel-roman@lenta.ru>
2021-12-02 20:10:37 +0700

    refactor: refactor mocks

--- src/components/pages/ChatPage/mocks.ts
+++ src/components/pages/ChatPage/mocks.ts
@@ -1,146 +1,28 @@
+import { UserInfo } from '../../../store/chatsStore';
+
 export const emptyResponse = [];
 
-interface Messages {
-  type: string;
-  source: string;
-  text: string;
-  title?: string;
-  size?: number;
-}
-
-export interface Response {
-  name: string;
-  gender: string;
-  id: string;
-  lastSeen: string;
-  messages: Messages[];
-}
-
-export const filledResponse: Response[] = [
+export const filledResponse: UserInfo[] = [
   {
     name: 'Konstantin Konstantinopolski',
     gender: 'male',
-    id: '000001',
-    lastSeen: '10',
-    messages: [
-      {
-        type: 'text',
-        source: 'incoming',
-        text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem',
-      },
-      {
-        type: 'text',
-        source: 'outcoming',
-        text: 'SeSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.  accusantium doloremque laudantium, totam re',
-      },
-      {
-        type: 'file',
-        source: 'incoming',
-        text: 'Pepega.png',
-        size: 4414141,
-        title: 'Pepega.png',
-      },
-      {
-        type: 'text',
-        source: 'incoming',
-        text: 'SeSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
-      },
-      {
-        type: 'text',
-        source: 'outcoming',
-        text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusanti',
-      },
-    ],
+    messages: [],
   },
   {
     name: 'Marina Joe',
     gender: 'female',
-    id: '000002',
-    lastSeen: '3',
-    messages: [
-      {
-        type: 'text',
-        source: 'incoming',
-        text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem',
-      },
-      {
-        type: 'text',
-        source: 'outcoming',
-        text: 'SeSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.  accusantium doloremque laudantium, totam re',
-      },
-      {
-        type: 'text',
-        source: 'outcoming',
-        text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusanti',
-      },
-      {
-        type: 'text',
-        source: 'incoming',
-        text: 'SeSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
-      },
-      {
-        type: 'file',
-        source: 'outcoming',
-        text: 'Pepega.png',
-        size: 4414141,
-        title: 'Pepega.png',
-      },
-    ],
+    messages: [],
   },
   {
     name: 'Ernest Gillroy',
     gender: 'male',
-    id: '000003',
-    lastSeen: '45',
-    messages: [
-      {
-        type: 'text',
-        source: 'incoming',
-        text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem',
-      },
-      {
-        type: 'text',
-        source: 'outcoming',
-        text: 'SeSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.  accusantium doloremque laudantium, totam re',
-      },
-      {
-        type: 'text',
-        source: 'incoming',
-        text: 'SeSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
-      },
-      {
-        type: 'text',
-        source: 'outcoming',
-        text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusanti',
-      },
-    ],
+    messages: [],
   },
   {
     name: 'Konstantin Konstantinopolski',
     gender: 'male',
     id: '000004',
     lastSeen: '55',
-    messages: [
-      {
-        type: 'text',
-        source: 'incoming',
-        text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem',
-      },
-      {
-        type: 'text',
-        source: 'outcoming',
-        text: 'SeSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.  accusantium doloremque laudantium, totam re',
-      },
-      {
-        type: 'text',
-        source: 'outcoming',
-        text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusanti',
-      },
-      {
-        type: 'text',
-        source: 'incoming',
-        text: 'SeSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
-      },
-    ],
+    messages: [],
   },
 ];
